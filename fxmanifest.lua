f9b4b0c3db24d491da2809cd25e9bf033128c0cb
Vincent <57841743+xXatawXx@users.noreply.github.com>
2021-01-25 17:59:24 -0800

    fxmanifest
    Resource Manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Devtag, I didn\'t create the NUI side.'
+version '1.1.0'
+
+client_script 'vin_devtag_cl.lua'
+server_script 'vin_devtag_sv.lua'
+
+files {
+	"vin_devtag.html",
+	"vin_devtag.js",
+	"option_1.png",
+	"option_2.png",
+	"option_3.png",
+}
+
+ui_page {
+	'vin_devtag.html',
+}
